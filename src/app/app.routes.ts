c3504ed37e47cdf3bf9b8e544ddd024efe4488b9
hareeshvs72 <hareeshvs72@gmail.com>
2025-12-09 21:44:07 +0530

    finish layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,14 @@
 import { Article } from "./article/article";
 import { Auth } from "./auth/auth";
+import { BlogTitle } from "./blog-title/blog-title";
 import { Dashboard } from "./dashboard/dashboard";
+import { GenerateImage } from "./generate-image/generate-image";
 import { Landing } from "./landing/landing";
 import { Layout } from "./layout/layout";
+import { RemoveBackground } from "./remove-background/remove-background";
 import { RemoveObject } from "./remove-object/remove-object";
 import { Routes } from '@angular/router';
+import { ReviewResume } from "./review-resume/review-resume";
 
 export const routes: Routes = [
 
@@ -18,7 +22,15 @@ export const routes: Routes = [
     children: [
       { path: "dashboard", component: Dashboard, title: "Dashboard" },
       { path: "removeObject", component: RemoveObject, title: "Remove Object" },
-      { path: "article", component: Article, title: "Write Article" }
+      { path: "article", component: Article, title: "Write Article" },
+         { path: "blog-title", component: BlogTitle, title: "Blog Title" },
+          { path: "image-generate", component: GenerateImage, title: "Image Generator" },
+           { path: "remove-background", component: RemoveBackground, title: "Background Remover" },
+              { path: "remove-object", component: RemoveObject, title: "Object Remover" },
+                 { path: "remove-background", component: RemoveBackground, title: "Background Remover" },
+                  { path: "resume-review", component: ReviewResume, title: "Review Resume" }
+                 
+
     ]
   },
 
--- src/app/article/article.ts
+++ src/app/article/article.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
 import { Sidebar } from '../sidebar/sidebar';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-article',
-  imports: [Sidebar],
+  standalone:true,
+  imports: [CommonModule],
   templateUrl: './article.html',
   styleUrl: './article.css',
 })
 export class Article {
+ articleLength:any = [
+  {length:300, text:"Short (300 words )"},
+  {length:900, text:"Short (900 words )"},
+  {length:1200, text:"Short (1200 words )"}
+]
+
+lengthSelect:number | null = null
+
+selectedLength(item:any){
+  this.lengthSelect = item.length
+ 
+}
 
 }
